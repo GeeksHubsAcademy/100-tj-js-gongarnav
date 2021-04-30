8cb2f8b987efad17fbef907068c542b06533888e
gonzalogcontacto <gonzalogcontacto@gmail.com>
2021-04-30 14:43:49 +0200

    * test01 passed locally

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,22 @@
 function geekshubs(index) {
-    return "";
+    var result = '';
+
+    for(i=1; i <=index; i++ ){    
+        
+        if (i % 3 == 0 && i % 5 != 0) {
+            result = result.concat("Geeks\n");
+        } 
+        else if (i % 5 == 0 && i % 3 != 0) {
+            result = result.concat("Hubs\n");
+        } 
+        else if (i % 5 == 0 && i % 3 == 0) {
+            result = result.concat("GeeksHubs\n");
+        } 
+        else{
+            result = result.concat(`${i}\n`)
+        }
+    }
+
+    return result;
 }
 module.exports = geekshubs;
